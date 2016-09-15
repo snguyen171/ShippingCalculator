3fa3f9888b607c0052a57f8e157c058328da74b9
snguyen171 <sonngn4@gmail.com>
2016-09-15 03:16:04 -0700

    layout complete minor code

--- app/src/main/java/edu/orangecoastcollege/cs273/occusername/snguyen171/shippingcalculator/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/occusername/snguyen171/shippingcalculator/MainActivity.java
@@ -2,12 +2,23 @@ package edu.orangecoastcollege.cs273.occusername.snguyen171.shippingcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText weightEditText
+    private TextView baseCostNum;
+    private TextView addedCostNum;
+    private TextView totalCostNum;
+
+    ShipItem currentItem = new ShipItem();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    weightEditText = (EditText) findViewById(R.id.weightEditText);
+
 }
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/occusername/snguyen171/shippingcalculator/ShipItem.java
@@ -0,0 +1,21 @@
+package edu.orangecoastcollege.cs273.occusername.snguyen171.shippingcalculator;
+
+/**
+ * Created by Nick on 9/15/2016.
+ */
+public class ShipItem {
+
+    private int mWeight;
+    private double mBase;
+    private double mAdded;
+    private double mTotal;
+
+    public ShipItem(){
+        mWeight = 0;
+        mBase = 0.0;
+        mAdded = 0.0;
+        mTotal = 0.0;
+    }
+
+
+}
