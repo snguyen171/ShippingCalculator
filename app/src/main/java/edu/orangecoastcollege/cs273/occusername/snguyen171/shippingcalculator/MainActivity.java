9ada36df76129f1e9b7b6f283cb68b95d1ed6c5e
snguyen171 <sonngn4@gmail.com>
2016-09-15 04:08:38 -0700

    more lines of code added.

--- app/src/main/java/edu/orangecoastcollege/cs273/occusername/snguyen171/shippingcalculator/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/occusername/snguyen171/shippingcalculator/MainActivity.java
@@ -2,23 +2,67 @@ package edu.orangecoastcollege.cs273.occusername.snguyen171.shippingcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.text.TextWatcher;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText weightEditText
+    private static NumberFormat currency = NumberFormat.getCurrencyInstance();
+
+    private EditText weightEditText;
     private TextView baseCostNum;
     private TextView addedCostNum;
     private TextView totalCostNum;
 
     ShipItem currentItem = new ShipItem();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        weightEditText = (EditText) findViewById(R.id.weightEditText);
+        baseCostNum = (TextView) findViewById(R.id.baseCostNum);
+        addedCostNum = (TextView) findViewById(R.id.addedCostNum);
+        totalCostNum = (TextView) findViewById(R.id.totalCostNum);
+
+        currentItem.setWeight(0);
     }
 
-    weightEditText = (EditText) findViewById(R.id.weightEditText);
+    private TextWatcher weightEditTextListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+            // Do nothing;
+        }
 
+        @Override
+        public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+            try{
+                if(charSequence.toString().isEmpty())
+                {
+                    currentItem.setWeight(0);
+                }
+                else
+                {
+                    int weight = Integer.parseInt(charSequence.toString());
+                    currentItem.setWeight(weight);
+                }
+            }
+            catch (NumberFormatException e)
+            {
+
+            }
+
+            baseCostNum.setText(currency.format)
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            // Do nothing;
+        }
+    };
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/occusername/snguyen171/shippingcalculator/ShipItem.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/occusername/snguyen171/shippingcalculator/ShipItem.java
@@ -17,5 +17,41 @@ public class ShipItem {
         mTotal = 0.0;
     }
 
+    public ShipItem(int mWeight)
+    {
+        this.mWeight = mWeight;
+        recalculateAmounts();
+    }
+
+    public int getWeight() {return mWeight; }
+    public int getBase() {return mBase; }
+    public int getAdded() {return mAdded; }
+    public int getTotal() {return mTotal; }
+
+    public void setWeight(int mWeight){
+        this.mWeight = mWeight;
+        recalculateAmounts();
+
+    }
+
+    private void recalculateAmounts()
+    {
+        if (mWeight >= 16)
+        {
+            mBase = 3.00;
+        }
+        else
+        {
+            mBase = 0.00;
+        }
+
+        if(mWeight > 16)
+        {
+            double additionWeight = (mWeight - 16)/4;
+            mAdded = Math.ceil(additionWeight) * 0.5;
+        }
+
+        mTotal = mBase + mAdded;
+    }
 
 }
